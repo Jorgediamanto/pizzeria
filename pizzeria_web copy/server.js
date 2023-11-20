19b02493f5857c0a78215ebf26db27d31a6382ac
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2023-11-20 01:05:37 +0100

    a

--- pizzeria_web copy/server.js
+++ pizzeria_web copy/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { exec } = require('child_process');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
 const app = express();
 const port = 5501;
 
@@ -14,20 +16,35 @@ app.post('/saveToCSV', (req, res) => {
 
     // Validate the input here if needed
 
-    // Execute the Python script with the user input
-    
-    const pythonScript = 'write_to_csv.py';
-    exec(`python ${pythonScript} ${userInput}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing Python script: ${error}`);
-            res.status(500).send('Error writing to pizzeria.csv');
-        } else {
-            console.log('Data has been written to pizzeria.csv');
-            console.log('Python script output:', stdout);
-            console.error('Python script errors:', stderr);
-            res.send('Data has been written to pizzeria.csv');
-        }
+    // Modificar el archivo CSV directamente en lugar de llamar al script de Python
+    const csvWriter = createCsvWriter({
+        path: 'public/pizzeria.csv',
+        header: [
+            { id: 'column1', title: 'Columna1' },
+            { id: 'column2', title: 'Columna2' },
+            // Agrega más columnas según tus necesidades
+        ],
+        append: true, // Para agregar a un archivo existente
     });
+
+    // Datos que deseas escribir en el CSV
+    const data = [
+        {
+            column1: 'Valor1',
+            column2: 'Valor2',
+            // Agrega más valores según tus necesidades
+        },
+    ];
+
+    csvWriter.writeRecords(data)
+        .then(() => {
+            console.log('Datos escritos en pizzeria.csv');
+            res.send('Datos escritos en pizzeria.csv');
+        })
+        .catch((error) => {
+            console.error(`Error escribiendo en pizzeria.csv: ${error}`);
+            res.status(500).send('Error escribiendo en pizzeria.csv');
+        });
 });
 
 app.listen(port, () => {
@@ -36,4 +53,4 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-  });
+});
